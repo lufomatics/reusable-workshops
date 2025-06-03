aa9712b6eb202edf9c812a0d2317851885029de5
Lucas Mello <lucas.mello@lufom.com>
2025-06-03 18:32:18 -0300

    Update set-git-aliases.sh

--- scripts/set-git-aliases.sh
+++ scripts/set-git-aliases.sh
@@ -10,8 +10,8 @@ EOF
 
 git config --global alias.sparse-clone '!bash -c '\''
   REPO=${1:-}; DIR=${2:-};
-  [ -z "$REPO" ] && read -p "Repo URL: REPO;
-  [ -z "$DIR"] && read -p "Dir to sparse-checkout: " DIR;
+  [ -z "$REPO" ] && read -p "Repo URL: " REPO;
+  [ -z "$DIR" ] && read -p "Dir to sparse-checkout: " DIR;
   git clone --filter=blob:none --sparse "$REPO"  && \
     cd "$(basename "$REPO" .git)" && \
     git sparse-checkout set "$DIR"
